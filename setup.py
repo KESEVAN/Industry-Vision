b6ff292f6a6f58b58a3648ebdfcf20a4b8456979
KESEVAN <kesevanbalaji@gmail.com>
2024-09-24 23:32:55 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'K7',
+    author_email= 'kesevanbalaji@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
